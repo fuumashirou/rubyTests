d409ab91353fae03cf9a706b3e6a677d4570f5d7
fuumashirou <s.matiassalgado@gmail.com>
2016-01-06 14:23:47 -0300

    Cambios para el test2

--- rubyTest2.rb
+++ rubyTest2.rb
@@ -1,22 +1,25 @@
 # suma los valores del array y devuelve las posiciones que al sumarlas sean igual al resultado ingresado
 def get_umbers_index(data_array, result)
-
-	data_array_copy = data_array.clone 
 	numbers_index = {}
+	total_elements = data_array.count
+	index_founded = 0
 
-	data_array.each do |index,data|
-		data_array_copy.each do |index_copy, data_copy|
-			if(data_array.index(data) != data_array.index(data_copy))
-				if ((data + data_copy) == result)
-					if(numbers_index.count == 0)
-						numbers_index = {:pos1 => data_array.index(data), :pos2 => data_array_copy.index(data_copy)}
-						puts numbers_index
-					end
-				end
+	data_array.each_with_index do |data, index|
+		aux_index = index +1
+
+		while(aux_index < total_elements)
+			if((data + data_array[aux_index]) == result)
+				index_founded = aux_index
 			end
+			aux_index += 1
 		end
-	end
 
-	numbers_index	
+		if (index_founded != 0)
+			puts "index = #{index} + index+1 = #{index_founded}"
+			numbers_index = {:pos1 => index, :pos2 => (index_founded)}
+			return numbers_index
+		end
+
+	end
 
 end
